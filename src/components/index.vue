bbec8327e5c3d7e7fc68eed25150dcd678f1a2a0
zhenjh <aihaozai@gmail.com>
2021-06-04 17:56:52 +0800

    基金图表

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <div v-for="(item,index) in fundData" :key="index" :id="item['fundCode']" style="height: 500px;width: 600px;">
+  <div class="block">
+    <div class="centered">
+      <div v-for="(item,index) in fundData" :key="index" :id="item['fundCode']" class="fundChart">
+        <span>{{item['fundName']}}（{{item['fundCode']}}）</span>
+      </div>
     </div>
   </div>
 </template>
@@ -9,6 +12,7 @@
 import { Chart } from '@antv/g2';
 import axios from 'axios';
 import service from '../config/service';
+import '@/assets/css/index.css'
 export default {
   name: 'index',
   mounted () {
@@ -17,9 +21,8 @@ export default {
     axios.get(service.SERVER_FUND + '/fund/fundReal/getFundRealData')
       .then(res => {
         const data = res.data.data;
-        console.log(data);
         for (const obj in data) {
-          __this.fundData.push({'fundCode': data[obj]['fundCode']})
+          __this.fundData.push({'fundCode': data[obj]['fundCode'], 'fundName': data[obj]['fundName']})
         }
         this.$nextTick(function () {
           for (const obj in data) {
@@ -28,37 +31,32 @@ export default {
               autoFit: true,
               height: 500,
               width: 600,
-              display: 'inline'
+              padding: [20, 20, 95, 80]
             });
 
             chart.data(data[obj]['fundRealList']);
             chart.scale({
               'gztime': {
-                nice: true
+                tickCount: 10
               },
               'gszzl': {
-                nice: true
+                tickCount: 10
               }
             });
-            // 格式化文字，超过长度添加省略号
-            // chart.axis('gztime', {
-            //   tickLine: null,
-            //   label: {
-            //     autoRotate: false,
-            //     autoHide: false, // 取消自动隐藏label
-            //     formatter (text) {
-            //       // 字符太长添加省略号
-            //       return text.length > 4 ? `${text.slice(0, 1)}` : text;
-            //     }
-            //   }
-            // });
+            // 配置自定义tooltip
             chart.tooltip({
-              showCrosshairs: true, // 展示 Tooltip 辅助线
-              shared: true
+              showContent: true,
+              showTitle: true,
+              containerTpl: '<div class="g2-tooltip"><div class="g2-tooltip-title"></div><div class="g2-tooltip-list"></div></div>',
+              itemTpl: '<div class="g2-tooltip-item">' +
+                '<div class="g2-tooltip-marker"></div>' +
+                '<div class="g2-tooltip-name" style="margin-bottom: 15px">估算涨跌百分比：{b}</div>' +
+                '<div class="g2-tooltip-value"></div>' +
+                '</div>'
             });
-
-            chart.line().position('gztime*gszzl')
-
+            chart.line().position('gztime*gszzl').tooltip('gztime*gszzl', (a, b) => {
+              return {a, b}
+            });
             chart.render();
           }
         })
@@ -75,6 +73,24 @@ export default {
 
 </script>
 
-<style scoped>
+<style scoped="scoped">
 
+  .block {
+    text-align: center;
+  }
+
+  /* The ghost, nudged to maintain perfect centering */
+  .block:before {
+    content: '';
+    display: inline-block;
+    height: 100%;
+    vertical-align: middle;
+    margin-right: -0.25em; /* Adjusts for spacing */
+  }
+
+  .centered {
+    display: inline-block;
+    vertical-align: middle;
+    width: 75%;
+  }
 </style>
